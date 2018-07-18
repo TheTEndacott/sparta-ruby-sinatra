77d24e3c5f115b53ad20aebdfc977c4d72968847
Tom Endacott <tendacott@spartaglobal.com>
2018-07-18 19:17:31 +0100

    Database linked to pre-existing file structure

--- controllers/posts_controller.rb
+++ controllers/posts_controller.rb
@@ -10,63 +10,88 @@ class PostsController < Sinatra::Base
   end
 
   # 'Temp demo dummy database'
-  $cars = [{
-     id: 0,
-     title: "Subaru",
-     body: "Impreza"
-  },
-  {
-      id: 1,
-      title: "Toyota",
-      body: "Supra"
-  },
-  {
-      id: 2,
-      title: "Nissan",
-      body: "Skyline"
-  }];
+  # $cars = [{
+  #    id: 0,
+  #    title: "Subaru",
+  #    body: "Impreza"
+  # },
+  # {
+  #     id: 1,
+  #     title: "Toyota",
+  #     body: "Supra"
+  # },
+  # {
+  #     id: 2,
+  #     title: "Nissan",
+  #     body: "Skyline"
+  # }];
 
   # INDEX
   get "/" do
     @title = "Cars Innit"
-    @cars = $cars
+    @posts = Post.all
     erb :"posts/index"
   end
 
   # NEW
   get "/new" do
+    @posts = {
+      id: "",
+      title: "",
+      body: ""
+    }
     erb :"posts/new"
   end
 
   # SHOW
   get "/:id" do
     id = params[:id].to_i
-    @cars = $cars[id]
+    @posts = Post.find(id)
+
     erb :"posts/show"
   end
 
   # CREATE
   post "/" do
-    "This is the create route!"
+    post = Post.new
+
+    post.title = params[:title]
+    post.body = params[:body]
+
+    post.save
+
+    redirect "/"
   end
 
   # EDIT
   get "/:id/edit" do
     id = params[:id]
-    "This is the edit route for #{id}!"
+    @posts = Post.find(id)
     erb :"posts/edit"
   end
 
   # UPDATE
   put "/:id" do
-    id = params[:id]
-    "This is the update route for #{id}!"
+    id = params[:id].to_i
+    post = Post.find(id)
+    post.title = params[:title]
+    post.body = params[:body]
+
+    post.save
+
+    redirect "/"
   end
 
   # DESTROY
   delete "/:id" do
-    id = params[:id]
-    "This is the destroy route for #{id}! <br> Destroy all humans!"
+    # Get the id
+    id = params[:id].to_i
+
+    # delete the post from the db
+    Post.destroy(id)
+
+    # redirect back to the homepage
+    redirect "/"
   end
 
 
